5ed03f0196899dc44e0693f045e0cefd2cc88be0
oscar066 <oscarkaruga1@gmail.com>
2025-06-02 16:44:26 +0300

    :construction: Complete V1 of the API

---
+++ api/__init__.py
@@ -0,0 +1,5 @@
+"""
+Fertility and Fertilizer Prediction API
+This module initializes the FastAPI application and sets up the endpoints for soil fertility and fertilizer prediction and
+search for nearest agrovet locations.
+"""
