4a779b57ca63e01d2ab89e33ef8459ce7b5c0380
oscar066 <oscarkaruga1@gmail.com>
2025-06-03 08:49:01 +0300

    :construction: login page as the landing page for kiduka

--- client/soil-app/src/app/page.tsx
+++ client/soil-app/src/app/page.tsx
@@ -1,6 +1,9 @@
 // import SoilFertilityDashboard from "./components/dashboard";
-import SoilFertilityDashboard from "./components/soil-fertility-dashboard"
+// import SoilFertilityDashboard from "./components/soil-fertility-dashboard"
 
-export default function Page() {
-  return <SoilFertilityDashboard />
+import React from "react";
+import LoginPage from "./components/login"
+
+export default function Login() {
+  return <LoginPage />
 }
