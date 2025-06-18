a5e53e7c599f38c1689cba5d605b0b8ce80c7489
oscar066 <oscarkaruga1@gmail.com>
2025-06-19 01:10:53 +0300

    :construction: add the MainDashboard

--- client/soil-app/src/app/dashboard/page.tsx
+++ client/soil-app/src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 
 import React from "react"
-import SoilFertilityDashboard from "../components/soil-fertility-dashboard";
+import MainDashboard from "../components/dashboard";
 
 export default function Page() {
-  return <SoilFertilityDashboard />
+  return <MainDashboard />
 }
