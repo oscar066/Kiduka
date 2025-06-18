8d1d68b754464097d0f6085e35c074d522c45cd0
oscar066 <oscarkaruga1@gmail.com>
2025-06-19 01:07:29 +0300

    :construction: require atleast 8 characters

--- client/soil-app/src/app/components/auth/signup.tsx
+++ client/soil-app/src/app/components/auth/signup.tsx
@@ -56,8 +56,8 @@ export default function SignupPage() {
       return;
     }
 
-    if (password.length < 6) {
-      setErrorMessage("Password must be at least 6 characters long");
+    if (password.length < 8) {
+      setErrorMessage("Password must be at least 8 characters long");
       setIsLoading(false);
       return;
     }
