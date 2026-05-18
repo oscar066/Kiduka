7289ce33e1f066e45243c38a0d5ca3e351e2ad43
Oscar Karuga <oscarkaruga1@gmail.com>
2026-05-18 19:27:33 +0300

    :bug: prerenders fix for admin pages

--- client/kiduka-app/src/app/admin/page.tsx
+++ client/kiduka-app/src/app/admin/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import React from "react";
 import { ProtectedPage } from "../components/auth/roleBasedGaurd";
 import { RoleBasedLayout } from "../components/layout/roleBasedLayout";
--- client/kiduka-app/src/app/admin/users/page.tsx
+++ client/kiduka-app/src/app/admin/users/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useState, useEffect } from "react";
 import { ProtectedPage } from "../../components/auth/roleBasedGaurd";
 import { AdminLayout } from "../../components/layout/roleBasedLayout";
